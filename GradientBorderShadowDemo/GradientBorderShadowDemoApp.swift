37d5b7492d5e0e643c45bc33f2ef7cc90c0e4720
Weerawut <weerawut@live.com>
2025-01-20 10:07:26 +0700

    Initial Commit

---
+++ GradientBorderShadowDemo/GradientBorderShadowDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientBorderShadowDemoApp.swift
+//  GradientBorderShadowDemo
+//
+//  Created by Weerawut Chaiyasomboon on 20/1/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct GradientBorderShadowDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
